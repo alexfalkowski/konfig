6b3443d45d6ece870f5728cec957da7826b6ba91
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-04-28 15:41:19 +0200

    test(server): move to v1

--- test/lib/konfig.rb
+++ test/lib/konfig.rb
@@ -32,13 +32,17 @@ module Konfig
     def health_grpc
       @health_grpc ||= Grpc::Health::V1::Health::Stub.new('localhost:9090', :this_channel_is_insecure)
     end
+  end
 
-    def server_http
-      @server_http ||= Konfig::V1::HTTP.new('http://localhost:8080')
-    end
+  module V1
+    class << self
+      def server_http
+        @server_http ||= Konfig::V1::HTTP.new('http://localhost:8080')
+      end
 
-    def server_grpc
-      @server_grpc ||= Konfig::V1::Service::Stub.new('localhost:9090', :this_channel_is_insecure)
+      def server_grpc
+        @server_grpc ||= Konfig::V1::Service::Stub.new('localhost:9090', :this_channel_is_insecure)
+      end
     end
   end
 end
