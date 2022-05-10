cccc39a1c89e21ae98ba23bf425e3fdec29c3803
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-05-10 16:26:15 +0200

    build(deps): update github.com/alexfalkowski/go-service v1.65.2

--- server/health/transport/http/observer.go
+++ server/health/transport/http/observer.go
@@ -3,11 +3,17 @@ package http
 import (
 	"github.com/alexfalkowski/go-health/server"
 	"github.com/alexfalkowski/go-service/health/transport/http"
+	"github.com/alexfalkowski/konfig/source"
 )
 
 // NewHealthObserver for HTTP.
-func NewHealthObserver(healthServer *server.Server) *http.HealthObserver {
-	return &http.HealthObserver{Observer: healthServer.Observe("noop")}
+func NewHealthObserver(healthServer *server.Server, source *source.Config) *http.HealthObserver {
+	names := []string{"vault"}
+	if source.IsGit() {
+		names = append(names, "git")
+	}
+
+	return &http.HealthObserver{Observer: healthServer.Observe(names...)}
 }
 
 // NewLivenessObserver for HTTP.
