1d38b21d13d4c351aca8a56a5b42cbda7d184b7c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-06-13 10:31:11 +0200

    refactor: rename to kind

--- source/config.go
+++ source/config.go
@@ -7,17 +7,17 @@ import (
 
 // Config for source.
 type Config struct {
-	Type   string        `yaml:"type"`
+	Kind   string        `yaml:"kind"`
 	Git    git.Config    `yaml:"git"`
 	Folder folder.Config `yaml:"folder"`
 }
 
 // IsGit configured.
 func (c *Config) IsGit() bool {
-	return c.Type == "git"
+	return c.Kind == "git"
 }
 
 // IsFolder configured.
 func (c *Config) IsFolder() bool {
-	return c.Type == "folder"
+	return c.Kind == "folder"
 }
