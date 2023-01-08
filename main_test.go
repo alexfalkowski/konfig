1c880bae3ea9251bbc025fe0f77720fa3b2741fe
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-01-08 23:23:04 +0100

    feat: update github.com/alexfalkowski/go-service to v1.81.0 (#395)

--- main_test.go
+++ main_test.go
@@ -4,19 +4,10 @@ package main
 
 import (
 	"testing"
-
-	scmd "github.com/alexfalkowski/go-service/cmd"
-	"github.com/alexfalkowski/konfig/cmd"
 )
 
 func TestFeatures(t *testing.T) {
-	command := scmd.New()
-
-	command.AddServer(cmd.ServerOptions)
-	command.AddClient(cmd.ClientOptions)
-	command.AddVersion(cmd.Version)
-
-	if err := command.Run(); err != nil {
+	if err := command().Run(); err != nil {
 		t.Fatal(err.Error())
 	}
 }
