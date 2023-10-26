230bf78b69372bd97459bd7caee34825081d8314
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-10-26 21:43:28 +0200

    feat(deps): update github.com/alexfalkowski/go-service to v1.100.0

--- provider/vault/telemetry/tracer/tracer.go
+++ provider/vault/telemetry/tracer/tracer.go
@@ -1,6 +1,7 @@
 package tracer
 
 import (
+	"github.com/alexfalkowski/go-service/env"
 	"github.com/alexfalkowski/go-service/telemetry/tracer"
 	"github.com/alexfalkowski/go-service/version"
 	"go.opentelemetry.io/otel/trace"
@@ -11,6 +12,6 @@ import (
 type Tracer trace.Tracer
 
 // NewTracer for tracer.
-func NewTracer(lc fx.Lifecycle, cfg *tracer.Config, version version.Version) (Tracer, error) {
-	return tracer.NewTracer(lc, "vault", version, cfg)
+func NewTracer(lc fx.Lifecycle, cfg *tracer.Config, env env.Environment, ver version.Version) (Tracer, error) {
+	return tracer.NewTracer(lc, "vault", env, ver, cfg)
 }
