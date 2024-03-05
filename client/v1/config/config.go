3773e04c96f016a95d50c4349325ec3166955cca
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-05 23:18:31 +0100

    feat(server): use client config (#999)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	"time"
+
+	"github.com/alexfalkowski/go-service/retry"
 )
 
 // Config for client.
 type Config struct {
 	Host        string        `yaml:"host" json:"host" toml:"host"`
 	Timeout     time.Duration `yaml:"timeout" json:"timeout" toml:"timeout"`
+	Retry       retry.Config  `yaml:"retry" json:"retry" toml:"retry"`
+	UserAgent   string        `yaml:"user_agent" json:"user_agent" toml:"user_agent"`
 	Application string        `yaml:"application" json:"application" toml:"application"`
 	Version     string        `yaml:"version" json:"version" toml:"version"`
 	Environment string        `yaml:"environment" json:"environment" toml:"environment"`
