62558f3c695953f6d418f1f68db4dea0e95abbe1
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-16 18:13:17 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.141.0

--- health/config.go
+++ health/config.go
@@ -1,11 +1,7 @@
 package health
 
-import (
-	"time"
-)
-
 // Config for health.
 type Config struct {
-	Duration time.Duration `yaml:"duration,omitempty" json:"duration,omitempty" toml:"duration,omitempty"`
-	Timeout  time.Duration `yaml:"timeout,omitempty" json:"timeout,omitempty" toml:"timeout,omitempty"`
+	Duration string `yaml:"duration,omitempty" json:"duration,omitempty" toml:"duration,omitempty"`
+	Timeout  string `yaml:"timeout,omitempty" json:"timeout,omitempty" toml:"timeout,omitempty"`
 }
--- server/health/registrations.go
+++ server/health/registrations.go
@@ -1,11 +1,13 @@
 package health
 
 import (
+	"time"
+
 	"github.com/alexfalkowski/go-health/checker"
 	"github.com/alexfalkowski/go-health/server"
 	"github.com/alexfalkowski/go-service/health"
 	"github.com/alexfalkowski/go-service/transport/http"
-	khealth "github.com/alexfalkowski/konfig/health"
+	h "github.com/alexfalkowski/konfig/health"
 	"github.com/alexfalkowski/konfig/source"
 	"github.com/hashicorp/vault/api"
 	"go.uber.org/fx"
@@ -18,24 +20,29 @@ type RegistrationsParams struct {
 	HTTP   *http.Config
 	Source *source.Config
 	Vault  *api.Config
-	Health *khealth.Config
+	Health *h.Config
 }
 
 // NewRegistrations for health.
 func NewRegistrations(params RegistrationsParams) (health.Registrations, error) {
+	d, err := time.ParseDuration(params.Health.Duration)
+	if err != nil {
+		return nil, err
+	}
+
 	client, err := http.NewClient(http.WithClientUserAgent(params.HTTP.UserAgent))
 	if err != nil {
 		return nil, err
 	}
 
 	registrations := health.Registrations{
-		server.NewRegistration("noop", params.Health.Duration, checker.NewNoopChecker()),
-		server.NewRegistration("vault", params.Health.Duration, checker.NewHTTPChecker(params.Vault.Address, client)),
+		server.NewRegistration("noop", d, checker.NewNoopChecker()),
+		server.NewRegistration("vault", d, checker.NewHTTPChecker(params.Vault.Address, client)),
 	}
 	s := params.Source
 
 	if s.IsGit() {
-		registrations = append(registrations, server.NewRegistration("git", params.Health.Duration, checker.NewHTTPChecker(s.Git.URL, client)))
+		registrations = append(registrations, server.NewRegistration("git", d, checker.NewHTTPChecker(s.Git.URL, client)))
 	}
 
 	return registrations, nil
