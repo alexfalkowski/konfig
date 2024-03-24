684d26e0c9c4acbdfb7df83e076b08b649e2cc22
Alex Falkowski <alexrfalkowski@gmail.com>
2024-03-24 12:59:18 +0100

    fix(config): check presense

--- config/configurator.go
+++ config/configurator.go
@@ -17,7 +17,12 @@ func NewConfigurator(i *cmd.InputConfig) (config.Configurator, error) {
 }
 
 func v1Client(cfg config.Configurator) *v1.Config {
-	return cfg.(*Config).Client.V1
+	c := cfg.(*Config)
+	if c.Client == nil {
+		return nil
+	}
+
+	return c.Client.V1
 }
 
 func v1AuthClientConfig(cfg config.Configurator) *av1.Config {
