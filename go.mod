8de6f80d8bc475bef2f1f79f9c3a1e5911b27d9b
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-04-01 10:57:37 +0200

    feat(deps): update github.com/alexfalkowski/go-service to v1.150.0 (#1085)

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/alexfalkowski/konfig
 go 1.22.1
 
 require (
-	github.com/alexfalkowski/auth v1.81.4
+	github.com/alexfalkowski/auth v1.82.0
 	github.com/alexfalkowski/go-health v1.16.0
-	github.com/alexfalkowski/go-service v1.149.4
+	github.com/alexfalkowski/go-service v1.150.0
 	github.com/aws/aws-sdk-go-v2 v1.26.1
 	github.com/aws/aws-sdk-go-v2/config v1.27.10
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.53.1
--- server/health/registrations.go
+++ server/health/registrations.go
@@ -1,11 +1,10 @@
 package health
 
 import (
-	"time"
-
 	"github.com/alexfalkowski/go-health/checker"
 	"github.com/alexfalkowski/go-health/server"
 	"github.com/alexfalkowski/go-service/health"
+	"github.com/alexfalkowski/go-service/time"
 	"github.com/alexfalkowski/go-service/transport/http"
 	h "github.com/alexfalkowski/konfig/health"
 	"github.com/alexfalkowski/konfig/source"
@@ -25,16 +24,12 @@ type RegistrationsParams struct {
 
 // NewRegistrations for health.
 func NewRegistrations(params RegistrationsParams) (health.Registrations, error) {
-	d, err := time.ParseDuration(params.Health.Duration)
-	if err != nil {
-		return nil, err
-	}
-
 	client, err := http.NewClient(http.WithClientUserAgent(params.HTTP.UserAgent))
 	if err != nil {
 		return nil, err
 	}
 
+	d := time.MustParseDuration(params.Health.Duration)
 	registrations := health.Registrations{
 		server.NewRegistration("noop", d, checker.NewNoopChecker()),
 		server.NewRegistration("vault", d, checker.NewHTTPChecker(params.Vault.Address, client)),
