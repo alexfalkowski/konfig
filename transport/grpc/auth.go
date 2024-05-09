94816a2dcc710c97a5ad96d7a7abf209d9b4a341
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-09 10:36:33 +0200

    fix(grpc): use os for name (#1218)

--- transport/grpc/auth.go
+++ transport/grpc/auth.go
@@ -2,22 +2,18 @@ package grpc
 
 import (
 	"github.com/alexfalkowski/auth/client"
+	"github.com/alexfalkowski/auth/security"
 	"github.com/alexfalkowski/go-service/os"
 	"github.com/alexfalkowski/go-service/security/token"
 	gt "github.com/alexfalkowski/go-service/transport/grpc/security/token"
 	"google.golang.org/grpc"
 )
 
-// IsAuth for GRPC.
-func IsAuth(c *token.Config) bool {
-	return c != nil && c.Kind == "auth"
-}
-
 // UnaryServerInterceptor for gRPC.
 func UnaryServerInterceptor(cfg *token.Config, tkn *client.Token) []grpc.UnaryServerInterceptor {
 	its := []grpc.UnaryServerInterceptor{}
 
-	if IsAuth(cfg) {
+	if security.IsAuth(cfg) {
 		its = append(its, gt.UnaryServerInterceptor(tkn.Verifier("jwt", os.ExecutableName(), "get-config")))
 	}
 
@@ -28,7 +24,7 @@ func UnaryServerInterceptor(cfg *token.Config, tkn *client.Token) []grpc.UnarySe
 func StreamServerInterceptor(cfg *token.Config, tkn *client.Token) []grpc.StreamServerInterceptor {
 	its := []grpc.StreamServerInterceptor{}
 
-	if IsAuth(cfg) {
+	if security.IsAuth(cfg) {
 		its = append(its, gt.StreamServerInterceptor(tkn.Verifier("jwt", os.ExecutableName(), "get-config")))
 	}
 
