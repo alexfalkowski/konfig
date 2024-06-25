baca01e2e3e5a1bd5b8343341d410e753bc446f6
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-25 07:50:29 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.221.0

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -1,22 +1,11 @@
 package http
 
 import (
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/net/http"
 	"github.com/alexfalkowski/konfig/server/service"
-	"go.uber.org/fx"
 )
 
 type (
-	// RegisterParams for HTTP.
-	RegisterParams struct {
-		fx.In
-
-		Marshaller *marshaller.Map
-		Mux        http.ServeMux
-		Service    *service.Service
-	}
-
 	// Server for HTTP.
 	Server struct {
 		service *service.Service
@@ -29,18 +18,9 @@ type (
 )
 
 // Register for HTTP.
-func Register(params RegisterParams) error {
-	s := &Server{service: params.Service}
+func Register(service *service.Service) {
+	s := &Server{service: service}
 
-	gh := http.NewHandler[GetConfigRequest](params.Mux, params.Marshaller, &configErrorer{})
-	if err := gh.Handle("POST", "/v1/config", s.GetConfig); err != nil {
-		return err
-	}
-
-	sh := http.NewHandler[GetSecretsRequest](params.Mux, params.Marshaller, &secretsErrorer{})
-	if err := sh.Handle("POST", "/v1/secrets", s.GetSecrets); err != nil {
-		return err
-	}
-
-	return nil
+	http.Handler("POST /v1/config", &configErrorer{}, s.GetConfig)
+	http.Handler("POST /v1/secrets", &secretsErrorer{}, s.GetSecrets)
 }
