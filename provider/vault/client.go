066d3f67732a3c1144e4ceed4917a9ce3ad96898
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-27 21:07:02 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.226.0

--- provider/vault/client.go
+++ provider/vault/client.go
@@ -25,7 +25,7 @@ type ConfigParams struct {
 func NewConfig(params ConfigParams) *api.Config {
 	client := http.NewClient(
 		http.WithClientLogger(params.Logger), http.WithClientTracer(params.Tracer),
-		http.WithClientMetrics(params.Meter), http.WithClientUserAgent(string(params.UserAgent)),
+		http.WithClientMetrics(params.Meter), http.WithClientUserAgent(params.UserAgent),
 	)
 
 	config := api.DefaultConfig()
