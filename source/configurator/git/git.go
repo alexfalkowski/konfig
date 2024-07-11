4cb474a075f76ca71d316bf5a0104385a225ef67
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-11 07:38:19 +0200

    feat(deps): upgrade github.com/google/go-github/v63 to v63.0.0 (#1368)

--- source/configurator/git/git.go
+++ source/configurator/git/git.go
@@ -9,7 +9,7 @@ import (
 	"github.com/alexfalkowski/go-service/telemetry/tracer"
 	"github.com/alexfalkowski/konfig/git"
 	ce "github.com/alexfalkowski/konfig/source/configurator/errors"
-	"github.com/google/go-github/v62/github"
+	"github.com/google/go-github/v63/github"
 	"go.opentelemetry.io/otel/trace"
 )
 
