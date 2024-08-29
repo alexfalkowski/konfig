b1630dc50787c850b6f599e00a55add24b35f4bb
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-29 14:30:42 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.284.0

--- test/features/step_definitions/server/health/transport/http/observability_steps.rb
+++ test/features/step_definitions/server/health/transport/http/observability_steps.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Given('I should see {string} as unhealthy') do |service|
+  opts = {
+    headers: { request_id: SecureRandom.uuid, content_type: :json, accept: :json },
+    read_timeout: 10, open_timeout: 10
+  }
+
   wait_for do
-    @response = Konfig.observability.health
+    @response = Konfig.observability.health(opts)
     @response.code
   end.to eq(503)
 
@@ -29,8 +34,13 @@ Then('the system should respond with metrics') do
 end
 
 Then('I should see {string} as healthy') do |service|
+  opts = {
+    headers: { request_id: SecureRandom.uuid, content_type: :json, accept: :json },
+    read_timeout: 10, open_timeout: 10
+  }
+
   wait_for do
-    @response = Konfig.observability.health
+    @response = Konfig.observability.health(opts)
     @response.code
   end.to eq(200)
 
