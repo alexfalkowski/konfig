312b9f5499dfd32db74ebfa5e0ee00592381592b
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-17 18:38:12 +0100

    test(api): update nonnative (1.84.1)
    https://github.com/alexfalkowski/nonnative/pull/467

--- test/features/step_definitions/health/transport/http/observability_steps.rb
+++ test/features/step_definitions/health/transport/http/observability_steps.rb
@@ -1,26 +1,12 @@
 # frozen_string_literal: true
 
-Given('I should see {string} as unhealthy') do |service|
-  opts = {
-    headers: { request_id: SecureRandom.uuid, content_type: :json, accept: :json },
-    read_timeout: 10, open_timeout: 10
-  }
-
-  wait_for do
-    @response = Konfig.observability.health(opts)
-    @response.code
-  end.to eq(503)
-
-  expect(@response.body).to include(service)
-end
-
 When('the system requests the {string} with HTTP') do |name|
   opts = {
     headers: { request_id: SecureRandom.uuid, content_type: :json, accept: :json },
     read_timeout: 10, open_timeout: 10
   }
 
-  @response = Konfig.observability.send(name, opts)
+  @response = Nonnative.observability.send(name, opts)
 end
 
 Then('the system should respond with a healthy status with HTTP') do
@@ -31,18 +17,4 @@ end
 Then('the system should respond with metrics') do
   expect(@response.code).to eq(200)
   expect(@response.body).to include('go_info')
-end
-
-Then('I should see {string} as healthy') do |service|
-  opts = {
-    headers: { request_id: SecureRandom.uuid, content_type: :json, accept: :json },
-    read_timeout: 10, open_timeout: 10
-  }
-
-  wait_for do
-    @response = Konfig.observability.health(opts)
-    @response.code
-  end.to eq(200)
-
-  expect(@response.body).to_not include(service)
 end
