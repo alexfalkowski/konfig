6d6559c5248c710b08f37f49c553d219f037d625
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-18 09:00:16 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0
    https://github.com/alexfalkowski/go-service/pull/1194

--- config/module.go
+++ config/module.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	"github.com/alexfalkowski/go-service/config"
-	"github.com/alexfalkowski/go-service/crypto"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
-	crypto.Module,
 	fx.Provide(config.NewConfig[Config]),
 	config.Module,
 	fx.Decorate(decorateConfig),
