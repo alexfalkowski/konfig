30ae638886e650edf8daaf1ecbe37dc3315cf02f
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-01-29 09:10:08 +0100

    test(deps): update nonnative (1.92.0) (#1845)
    https://github.com/alexfalkowski/nonnative/pull/503

--- test/lib/konfig/v1/http.rb
+++ test/lib/konfig/v1/http.rb
@@ -3,13 +3,26 @@
 module Konfig
   module V1
     class HTTP < Nonnative::HTTPClient
+      def initialize(host)
+        @tries = 3
+        @wait = 0.5
+
+        super
+      end
+
       def get_config(params, opts = {})
-        post('/v1/config', params.to_json, opts)
+        with_retry(tries, wait) do
+          post('/v1/config', params.to_json, opts)
+        end
       end
 
       def get_secrets(params, opts = {})
         post('/v1/secrets', { secrets: params }.to_json, opts)
       end
+
+      private
+
+      attr_reader :tries, :wait
     end
   end
 end
